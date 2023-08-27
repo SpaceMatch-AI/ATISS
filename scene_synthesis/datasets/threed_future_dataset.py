b091b0df36d5331b054c09507f022d9ec0f92979
AfoninAndrei <andrei@andrei.northamerica-northeast2-a.c.intelligent-map-390817.internal>
2023-08-27 10:57:21 +0000

    add support for topk geneeration each step

--- scene_synthesis/datasets/threed_future_dataset.py
+++ scene_synthesis/datasets/threed_future_dataset.py
@@ -1,10 +1,10 @@
-# 
+#
 # Copyright (C) 2021 NVIDIA Corporation.  All rights reserved.
 # Licensed under the NVIDIA Source Code License.
 # See LICENSE at https://github.com/nv-tlabs/ATISS.
 # Authors: Despoina Paschalidou, Amlan Kar, Maria Shugrina, Karsten Kreis,
 #          Andreas Geiger, Sanja Fidler
-# 
+#
 
 import numpy as np
 import pickle
@@ -13,6 +13,7 @@ from .utils import parse_threed_future_models
 
 
 class ThreedFutureDataset(object):
+
     def __init__(self, objects):
         assert len(objects) > 0
         self.objects = objects
@@ -22,8 +23,7 @@ class ThreedFutureDataset(object):
 
     def __str__(self):
         return "Dataset contains {} objects with {} discrete types".format(
-            len(self)
-        )
+            len(self))
 
     def __getitem__(self, idx):
         return self.objects[idx]
@@ -31,34 +31,30 @@ class ThreedFutureDataset(object):
     def _filter_objects_by_label(self, label):
         return [oi for oi in self.objects if oi.label == label]
 
-    def get_closest_furniture_to_box(self, query_label, query_size):
+    def get_closest_furniture_to_box(self, query_label, query_size, topk=1):
         objects = self._filter_objects_by_label(query_label)
 
         mses = {}
         for i, oi in enumerate(objects):
             mses[oi] = np.sum((oi.size - query_size)**2, axis=-1)
-        sorted_mses = [k for k, v in sorted(mses.items(), key=lambda x:x[1])]
-        return sorted_mses[0]
+        sorted_mses = [k for k, v in sorted(mses.items(), key=lambda x: x[1])]
+        return sorted_mses[:topk]
 
     def get_closest_furniture_to_2dbox(self, query_label, query_size):
         objects = self._filter_objects_by_label(query_label)
 
         mses = {}
         for i, oi in enumerate(objects):
-            mses[oi] = (
-                (oi.size[0] - query_size[0])**2 +
-                (oi.size[2] - query_size[1])**2
-            )
+            mses[oi] = ((oi.size[0] - query_size[0])**2 +
+                        (oi.size[2] - query_size[1])**2)
         sorted_mses = [k for k, v in sorted(mses.items(), key=lambda x: x[1])]
         return sorted_mses[0]
 
     @classmethod
-    def from_dataset_directory(
-        cls, dataset_directory, path_to_model_info, path_to_models
-    ):
-        objects = parse_threed_future_models(
-            dataset_directory, path_to_models, path_to_model_info
-        )
+    def from_dataset_directory(cls, dataset_directory, path_to_model_info,
+                               path_to_models):
+        objects = parse_threed_future_models(dataset_directory, path_to_models,
+                                             path_to_model_info)
         return cls(objects)
 
     @classmethod
